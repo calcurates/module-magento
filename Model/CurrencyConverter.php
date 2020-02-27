30828964d89baecb6540608bd3f2535e9a18966a
NikolasSumrak <nikolassumrak@gmail.com>
2020-02-27 23:10:41 +0300

    [SAAS-730] fixed currency converter and some refactoring

---
+++ Model/CurrencyConverter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Calcurates\ModuleMagento\Model;
+
+use Magento\Directory\Model\Currency;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class CurrencyConverter
+{
+    /**
+     * @var PriceCurrencyInterface
+     */
+    private $priceCurrency;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * CurrencyConverter constructor.
+     * @param PriceCurrencyInterface $priceCurrency
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(PriceCurrencyInterface $priceCurrency, StoreManagerInterface $storeManager)
+    {
+        $this->priceCurrency = $priceCurrency;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * @param float $amount
+     * @param string $fromCurrencyCode
+     * @return float
+     */
+    public function convertToBase($amount, $fromCurrencyCode)
+    {
+        $baseAmount = $amount;
+
+        if (!$fromCurrencyCode) {
+            return $baseAmount;
+        }
+
+        try {
+            /** @var Currency $baseCurrency */
+            $baseCurrency = $this->storeManager->getStore()->getBaseCurrency();
+            $baseCurrencyCode = $baseCurrency->getCurrencyCode();
+            if ($baseCurrencyCode != $fromCurrencyCode) {
+                /** @var Currency $currency */
+                $currency = $this->priceCurrency->getCurrency(null, $fromCurrencyCode);
+                $rate = $currency->getRate($baseCurrencyCode);
+                if (!$rate) {
+                    $rates = $currency->getRates();
+                    $rates[$baseCurrencyCode] = $baseCurrency->getRate($fromCurrencyCode)
+                        ? 1 / $baseCurrency->getRate($fromCurrencyCode)
+                        : 1;
+                    $currency->setRates($rates);
+                }
+
+                $baseAmount = $currency->convert($baseAmount, $baseCurrencyCode);
+            }
+        } catch (\Throwable $e) {
+            // do nothing
+        }
+
+        return $baseAmount;
+    }
+}
