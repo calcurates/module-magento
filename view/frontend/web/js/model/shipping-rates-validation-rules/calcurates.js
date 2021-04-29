797a62a23a8a0159093a6a66540f14a49e7d75fb
NikolasSumrak <nikolassumrak@gmail.com>
2021-04-29 13:55:07 +0300

    [SAAS-2020] removed unnecessary fields from update shipping rates trigger

--- view/frontend/web/js/model/shipping-rates-validation-rules/calcurates.js
+++ view/frontend/web/js/model/shipping-rates-validation-rules/calcurates.js
@@ -14,18 +14,6 @@ define([], function () {
          */
         getRules: function () {
             return {
-                'firstname': {
-                    'required': false
-                },
-                'lastname': {
-                    'required': false
-                },
-                'company': {
-                    'required': false
-                },
-                'telephone': {
-                    'required': false
-                },
                 'postcode': {
                     'required': true
                 },
