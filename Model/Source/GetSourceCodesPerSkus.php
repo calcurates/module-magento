6d9947e8e4a57d498753f7ac98e36cf9e722745a
Gemorroj <wapinet@mail.ru>
2022-01-06 17:06:21 +0300

    build: cs fix

--- Model/Source/GetSourceCodesPerSkus.php
+++ Model/Source/GetSourceCodesPerSkus.php
@@ -1,13 +1,12 @@
 <?php
-
-declare(strict_types=1);
-
 /**
  * @author Calcurates Team
  * @copyright Copyright © 2020 Calcurates (https://www.calcurates.com)
  * @license https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @package Calcurates_ModuleMagento
  */
+
+declare(strict_types=1);
 
 namespace Calcurates\ModuleMagento\Model\Source;
 
