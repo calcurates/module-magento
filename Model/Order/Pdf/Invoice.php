8bc621611c32012e9d032796d6bb98be96db9579
Alexey Rakitin <alexey.rakitin@babenkocommerce.com>
2022-03-29 11:17:31 +0300

    SAAS-2129: Split shipments in PDF invoice, creditmemo

---
+++ Model/Order/Pdf/Invoice.php
@@ -0,0 +1,374 @@
+<?php
+
+namespace Calcurates\ModuleMagento\Model\Order\Pdf;
+
+use Calcurates\ModuleMagento\ViewModel\OrderSplitShipments;
+use IntlDateFormatter;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Framework\Stdlib\StringUtils;
+use Magento\Framework\Translate\Inline\StateInterface;
+use Magento\Payment\Helper\Data;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Address\Renderer;
+use Magento\Sales\Model\Order\Pdf\Config;
+use Magento\Sales\Model\Order\Pdf\Invoice as MagentoInvoice;
+use Magento\Sales\Model\Order\Pdf\ItemsFactory;
+use Magento\Sales\Model\Order\Pdf\Total\Factory;
+use Magento\Sales\Model\Order\Shipment;
+use Magento\Sales\Model\RtlTextHandler;
+use Magento\Store\Model\App\Emulation;
+use Magento\Store\Model\StoreManagerInterface;
+use Zend_Pdf_Color_GrayScale;
+use Zend_Pdf_Color_Rgb;
+
+class Invoice extends MagentoInvoice
+{
+    /**
+     * @var RtlTextHandler
+     */
+    private $rtlTextHandler;
+
+    /**
+     * @var OrderSplitShipments
+     */
+    private $viewModel;
+
+    /**
+     * @param Data $paymentData
+     * @param StringUtils $string
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Filesystem $filesystem
+     * @param Config $pdfConfig
+     * @param Factory $pdfTotalFactory
+     * @param ItemsFactory $pdfItemsFactory
+     * @param TimezoneInterface $localeDate
+     * @param StateInterface $inlineTranslation
+     * @param Renderer $addressRenderer
+     * @param StoreManagerInterface $storeManager
+     * @param Emulation $appEmulation
+     * @param OrderSplitShipments $viewModel
+     * @param array $data
+     * @param RtlTextHandler|null $rtlTextHandler
+     */
+    public function __construct(
+        Data                  $paymentData,
+        StringUtils           $string,
+        ScopeConfigInterface  $scopeConfig,
+        Filesystem            $filesystem,
+        Config                $pdfConfig,
+        Factory               $pdfTotalFactory,
+        ItemsFactory          $pdfItemsFactory,
+        TimezoneInterface     $localeDate,
+        StateInterface        $inlineTranslation,
+        Renderer              $addressRenderer,
+        StoreManagerInterface $storeManager,
+        Emulation             $appEmulation,
+        OrderSplitShipments   $viewModel,
+        array                 $data = [],
+        ?RtlTextHandler       $rtlTextHandler = null
+    ) {
+        $this->rtlTextHandler = $rtlTextHandler ?: ObjectManager::getInstance()->get(RtlTextHandler::class);
+        $this->viewModel = $viewModel;
+        parent::__construct(
+            $paymentData,
+            $string,
+            $scopeConfig,
+            $filesystem,
+            $pdfConfig,
+            $pdfTotalFactory,
+            $pdfItemsFactory,
+            $localeDate,
+            $inlineTranslation,
+            $addressRenderer,
+            $storeManager,
+            $appEmulation,
+            $data
+        );
+    }
+
+    protected function insertOrder(&$page, $obj, $putOrderId = true)
+    {
+        if ($obj instanceof Order) {
+            $shipment = null;
+            $order = $obj;
+        } elseif ($obj instanceof Shipment) {
+            $shipment = $obj;
+            $order = $shipment->getOrder();
+        }
+        $this->viewModel->setOrder($order);
+
+        $this->y = $this->y ?: 815;
+        $top = $this->y;
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0.45));
+        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.45));
+        $page->drawRectangle(25, $top, 570, $top - 55);
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $this->setDocHeaderCoordinates([25, $top, 570, $top - 55]);
+        $this->_setFontRegular($page, 10);
+
+        if ($putOrderId) {
+            $page->drawText(__('Order # ') . $order->getRealOrderId(), 35, $top -= 30, 'UTF-8');
+            $top += 15;
+        }
+
+        $top -= 30;
+        $page->drawText(
+            __('Order Date: ') .
+            $this->_localeDate->formatDate(
+                $this->_localeDate->scopeDate(
+                    $order->getStore(),
+                    $order->getCreatedAt(),
+                    true
+                ),
+                IntlDateFormatter::MEDIUM,
+                false
+            ),
+            35,
+            $top,
+            'UTF-8'
+        );
+
+        $top -= 10;
+        $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+        $page->setLineWidth(0.5);
+        $page->drawRectangle(25, $top, 275, $top - 25);
+        $page->drawRectangle(275, $top, 570, $top - 25);
+
+        /* Calculate blocks info */
+
+        /* Billing Address */
+        $billingAddress = $this->_formatAddress($this->addressRenderer->format($order->getBillingAddress(), 'pdf'));
+
+        /* Payment */
+        $paymentInfo = $this->_paymentData->getInfoBlock($order->getPayment())->setIsSecureMode(true)->toPdf();
+        $paymentInfo = htmlspecialchars_decode($paymentInfo, ENT_QUOTES);
+        $payment = explode('{{pdf_row_separator}}', $paymentInfo);
+        foreach ($payment as $key => $value) {
+            if (strip_tags(trim($value)) == '') {
+                unset($payment[$key]);
+            }
+        }
+        reset($payment);
+
+        /* Shipping Address and Method */
+        if (!$order->getIsVirtual()) {
+            /* Shipping Address */
+            $shippingAddress = $this->_formatAddress(
+                $this->addressRenderer->format($order->getShippingAddress(), 'pdf')
+            );
+            $shippingMethod = $order->getShippingDescription();
+        }
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontBold($page, 12);
+        $page->drawText(__('Sold to:'), 35, $top - 15, 'UTF-8');
+
+        if (!$order->getIsVirtual()) {
+            $page->drawText(__('Ship to:'), 285, $top - 15, 'UTF-8');
+        } else {
+            $page->drawText(__('Payment Method:'), 285, $top - 15, 'UTF-8');
+        }
+
+        $addressesHeight = $this->_calcAddressHeight($billingAddress);
+        if (isset($shippingAddress)) {
+            $addressesHeight = max($addressesHeight, $this->_calcAddressHeight($shippingAddress));
+        }
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $page->drawRectangle(25, $top - 25, 570, $top - 33 - $addressesHeight);
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontRegular($page, 10);
+        $this->y = $top - 40;
+        $addressesStartY = $this->y;
+
+        foreach ($billingAddress as $value) {
+            if ($value !== '') {
+                $text = [];
+                foreach ($this->string->split($value, 45, true, true) as $_value) {
+                    $text[] = $this->rtlTextHandler->reverseRtlText($_value);
+                }
+                foreach ($text as $part) {
+                    $page->drawText(strip_tags(ltrim($part)), 35, $this->y, 'UTF-8');
+                    $this->y -= 15;
+                }
+            }
+        }
+
+        $addressesEndY = $this->y;
+
+        if (!$order->getIsVirtual()) {
+            $this->y = $addressesStartY;
+            foreach ($shippingAddress as $value) {
+                if ($value !== '') {
+                    $text = [];
+                    foreach ($this->string->split($value, 45, true, true) as $_value) {
+                        $text[] = $this->rtlTextHandler->reverseRtlText($_value);
+                    }
+                    foreach ($text as $part) {
+                        $page->drawText(strip_tags(ltrim($part)), 285, $this->y, 'UTF-8');
+                        $this->y -= 15;
+                    }
+                }
+            }
+
+            $addressesEndY = min($addressesEndY, $this->y);
+            $this->y = $addressesEndY;
+
+            $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+            $page->setLineWidth(0.5);
+            $page->drawRectangle(25, $this->y, 275, $this->y - 25);
+            $page->drawRectangle(275, $this->y, 570, $this->y - 25);
+
+            $this->y -= 15;
+            $this->_setFontBold($page, 12);
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+            $page->drawText(__('Payment Method:'), 35, $this->y, 'UTF-8');
+            $page->drawText(__('Shipping Method:'), 285, $this->y, 'UTF-8');
+
+            $this->y -= 10;
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+
+            $this->_setFontRegular($page, 10);
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+
+            $paymentLeft = 35;
+            $yPayments = $this->y - 15;
+        } else {
+            $yPayments = $addressesStartY;
+            $paymentLeft = 285;
+        }
+
+        foreach ($payment as $value) {
+            if (trim($value) != '') {
+                //Printing "Payment Method" lines
+                $value = preg_replace('/<br[^>]*>/i', "\n", $value);
+                foreach ($this->string->split($value, 45, true, true) as $_value) {
+                    $page->drawText(strip_tags(trim($_value)), $paymentLeft, $yPayments, 'UTF-8');
+                    $yPayments -= 15;
+                }
+            }
+        }
+
+        if ($order->getIsVirtual()) {
+            // replacement of Shipments-Payments rectangle block
+            $yPayments = min($addressesEndY, $yPayments);
+            $page->drawLine(25, $top - 25, 25, $yPayments);
+            $page->drawLine(570, $top - 25, 570, $yPayments);
+            $page->drawLine(25, $yPayments, 570, $yPayments);
+
+            $this->y = $yPayments - 15;
+        } elseif ($this->viewModel->isSplitShipment()) {
+            $yShipments = $this->y;
+            $yShipments -= 15;
+            $shipmentCount = count($this->viewModel->getOrderData()->getSplitShipments());
+            foreach ($this->viewModel->getOrderData()->getSplitShipments() as $key => $splitShipment) {
+                $this->_setFontBold($page, 10);
+                $page->drawText(
+                    __('Shipment %1 of %2:', $key + 1, $shipmentCount),
+                    285,
+                    $yShipments,
+                    'UTF-8'
+                );
+                $yShipments -= 15;
+                $this->_setFontRegular($page, 10);
+
+                $methodTitle = $splitShipment['title'] ?? '';
+                if (trim($methodTitle) != '') {
+                    $methodTitle = preg_replace('/<br[^>]*>/i', "\n", $methodTitle);
+                    foreach ($this->string->split($methodTitle, 60, true, true) as $_value) {
+                        $page->drawText(strip_tags(trim($_value)), 285, $yShipments, 'UTF-8');
+                        $yShipments -= 15;
+                    }
+                }
+
+                $skusText = __('Included SKUs: %1', implode(', ', $splitShipment['products'] ?? []));
+                if (trim($skusText) != '') {
+                    $skusText = preg_replace('/<br[^>]*>/i', "\n", $skusText);
+                    foreach ($this->string->split($skusText, 60, true, true) as $_value) {
+                        $page->drawText(strip_tags(trim($_value)), 285, $yShipments, 'UTF-8');
+                        $yShipments -= 15;
+                    }
+                }
+            }
+            $currentY = min($yPayments, $yShipments);
+
+            // replacement of Shipments-Payments rectangle block
+            $page->drawLine(25, $this->y, 25, $currentY);
+            //left
+            $page->drawLine(25, $currentY, 570, $currentY);
+            //bottom
+            $page->drawLine(570, $currentY, 570, $this->y);
+            //right
+            $this->y = $currentY;
+            $this->y -= 15;
+        } else {
+            $topMargin = 15;
+            $methodStartY = $this->y;
+            $this->y -= 15;
+
+            foreach ($this->string->split($shippingMethod, 45, true, true) as $_value) {
+                $page->drawText(strip_tags(trim($_value)), 285, $this->y, 'UTF-8');
+                $this->y -= 15;
+            }
+
+            $yShipments = $this->y;
+            $totalShippingChargesText = "("
+                . __('Total Shipping Charges')
+                . " "
+                . $order->formatPriceTxt($order->getShippingAmount())
+                . ")";
+
+            $page->drawText($totalShippingChargesText, 285, $yShipments - $topMargin, 'UTF-8');
+            $yShipments -= $topMargin + 10;
+
+            $tracks = [];
+            if ($shipment) {
+                $tracks = $shipment->getAllTracks();
+            }
+            if (count($tracks)) {
+                $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+                $page->setLineWidth(0.5);
+                $page->drawRectangle(285, $yShipments, 510, $yShipments - 10);
+                $page->drawLine(400, $yShipments, 400, $yShipments - 10);
+                //$page->drawLine(510, $yShipments, 510, $yShipments - 10);
+
+                $this->_setFontRegular($page, 9);
+                $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+                //$page->drawText(__('Carrier'), 290, $yShipments - 7 , 'UTF-8');
+                $page->drawText(__('Title'), 290, $yShipments - 7, 'UTF-8');
+                $page->drawText(__('Number'), 410, $yShipments - 7, 'UTF-8');
+
+                $yShipments -= 20;
+                $this->_setFontRegular($page, 8);
+                foreach ($tracks as $track) {
+                    $maxTitleLen = 45;
+                    $endOfTitle = strlen($track->getTitle()) > $maxTitleLen ? '...' : '';
+                    $truncatedTitle = substr($track->getTitle(), 0, $maxTitleLen) . $endOfTitle;
+                    $page->drawText($truncatedTitle, 292, $yShipments, 'UTF-8');
+                    $page->drawText($track->getNumber(), 410, $yShipments, 'UTF-8');
+                    $yShipments -= $topMargin - 5;
+                }
+            } else {
+                $yShipments -= $topMargin - 5;
+            }
+
+            $currentY = min($yPayments, $yShipments);
+
+            // replacement of Shipments-Payments rectangle block
+            $page->drawLine(25, $methodStartY, 25, $currentY);
+            //left
+            $page->drawLine(25, $currentY, 570, $currentY);
+            //bottom
+            $page->drawLine(570, $currentY, 570, $methodStartY);
+            //right
+
+            $this->y = $currentY;
+            $this->y -= 15;
+        }
+    }
+}
