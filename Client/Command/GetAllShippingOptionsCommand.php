709d2671413cae02bfd93ee60ef6aa75382adff9
Vsevolod Tarasov <vsevolod@babenkocommerce.com>
2022-04-11 13:36:29 +0300

    Fixed SAAS-3094: added API exception handling

--- Client/Command/GetAllShippingOptionsCommand.php
+++ Client/Command/GetAllShippingOptionsCommand.php
@@ -40,53 +40,59 @@ class GetAllShippingOptionsCommand
 
         $shippingOptions = [];
 
-        foreach ($allShippingOptions['tableRates'] as $shippingOption) {
-            foreach ($shippingOption['methods'] as $method) {
-                $key = ShippingMethodManager::TABLE_RATE . '_' . $shippingOption['id'] . '_' .
-                    $method['id'];
-                $title = $shippingOption['shippingOption']['name'] . ' - ' . $method['name'];
-                $shippingOptions[$key] = $title;
-            }
-        }
-
-        foreach ($allShippingOptions['inStorePickups'] as $shippingOption) {
-            foreach ($shippingOption['stores'] as $store) {
-                $key = ShippingMethodManager::IN_STORE_PICKUP . '_' . $shippingOption['id'] . '_' .
-                    $store['id'];
-                $title = $shippingOption['shippingOption']['name'] . ' - ' . $store['name'];
-                $shippingOptions[$key] = $title;
-            }
-        }
-
-        foreach ($allShippingOptions['flatRates'] as $shippingOption) {
-            $key = ShippingMethodManager::FLAT_RATES . '_' . $shippingOption['id'];
-            $title = $shippingOption['shippingOption']['name'];
-            $shippingOptions[$key] = $title;
-        }
-
-        foreach ($allShippingOptions['freeShipping'] as $shippingOption) {
-            $key = ShippingMethodManager::FREE_SHIPPING . '_' . $shippingOption['id'];
-            $title = $shippingOption['shippingOption']['name'];
-            $shippingOptions[$key] = $title;
-        }
-
-        foreach ($allShippingOptions['carriers'] as $shippingOption) {
-            foreach ($shippingOption['services'] as $method) {
-                $key = ShippingMethodManager::CARRIER . '_' . $shippingOption['id'] .
-                    '_' . $method['id'];
-                $title = $shippingOption['shippingOption']['name'] . ' - ' . $method['name'];
-                $shippingOptions[$key] = $title;
-            }
-        }
-
-        foreach ($allShippingOptions['rateShopping'] as $shippingOption) {
-            foreach ($shippingOption['carriers'] as $carrier) {
-                foreach ($carrier['services'] as $method) {
-                    $key = ShippingMethodManager::RATE_SHOPPING . '_' . $shippingOption['id'] .
-                        '_' . $carrier['id'] . '_' . $method['id'];
-                    $title = $shippingOption['shippingOption']['name'] . ' - ' . $carrier['carrierName']
-                        . ' - ' . $method['name'];
+        if (0 === count(array_diff(
+                ['tableRates', 'inStorePickups', 'flatRates', 'freeShipping', 'carriers', 'rateShopping'],
+                array_keys($allShippingOptions)
+            ))
+        ) {
+            foreach ($allShippingOptions['tableRates'] as $shippingOption) {
+                foreach ($shippingOption['methods'] as $method) {
+                    $key = ShippingMethodManager::TABLE_RATE . '_' . $shippingOption['id'] . '_' .
+                        $method['id'];
+                    $title = $shippingOption['shippingOption']['name'] . ' - ' . $method['name'];
                     $shippingOptions[$key] = $title;
+                }
+            }
+
+            foreach ($allShippingOptions['inStorePickups'] as $shippingOption) {
+                foreach ($shippingOption['stores'] as $store) {
+                    $key = ShippingMethodManager::IN_STORE_PICKUP . '_' . $shippingOption['id'] . '_' .
+                        $store['id'];
+                    $title = $shippingOption['shippingOption']['name'] . ' - ' . $store['name'];
+                    $shippingOptions[$key] = $title;
+                }
+            }
+
+            foreach ($allShippingOptions['flatRates'] as $shippingOption) {
+                $key = ShippingMethodManager::FLAT_RATES . '_' . $shippingOption['id'];
+                $title = $shippingOption['shippingOption']['name'];
+                $shippingOptions[$key] = $title;
+            }
+
+            foreach ($allShippingOptions['freeShipping'] as $shippingOption) {
+                $key = ShippingMethodManager::FREE_SHIPPING . '_' . $shippingOption['id'];
+                $title = $shippingOption['shippingOption']['name'];
+                $shippingOptions[$key] = $title;
+            }
+
+            foreach ($allShippingOptions['carriers'] as $shippingOption) {
+                foreach ($shippingOption['services'] as $method) {
+                    $key = ShippingMethodManager::CARRIER . '_' . $shippingOption['id'] .
+                        '_' . $method['id'];
+                    $title = $shippingOption['shippingOption']['name'] . ' - ' . $method['name'];
+                    $shippingOptions[$key] = $title;
+                }
+            }
+
+            foreach ($allShippingOptions['rateShopping'] as $shippingOption) {
+                foreach ($shippingOption['carriers'] as $carrier) {
+                    foreach ($carrier['services'] as $method) {
+                        $key = ShippingMethodManager::RATE_SHOPPING . '_' . $shippingOption['id'] .
+                            '_' . $carrier['id'] . '_' . $method['id'];
+                        $title = $shippingOption['shippingOption']['name'] . ' - ' . $carrier['carrierName']
+                            . ' - ' . $method['name'];
+                        $shippingOptions[$key] = $title;
+                    }
                 }
             }
         }
