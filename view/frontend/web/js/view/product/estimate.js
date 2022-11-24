b742db24e3d144ddbbcc576571f2459e4ab23759
Vsevolod Tarasov <vsevolod@babenkocommerce.com>
2022-11-24 19:45:47 +0200

    Fixed SAAS-3769: adde valid cutoff time calculation

--- view/frontend/web/js/view/product/estimate.js
+++ view/frontend/web/js/view/product/estimate.js
@@ -179,11 +179,14 @@ define([
          */
         countdown: function (cutoff, element, initialTmpl) {
             let now = new Date(),
-                interval = new Date(cutoff - now),
+                interval = cutoff.getTime() - now.getTime(),
+                hours = Math.floor(interval / 3600000),
+                minutes = Math.floor(interval / 60000) - hours * 60,
+                seconds = Math.floor(interval / 1000) - hours * 3600 - minutes * 60,
                 tmpl = mageTemplate(this.timeTmplString, {
-                    hours: interval.getHours(),
-                    minutes: interval.getMinutes(),
-                    seconds: interval.getSeconds()
+                    hours: hours,
+                    minutes: minutes,
+                    seconds: seconds
                 })
             if (interval <= 0) {
                 return
