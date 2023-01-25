f5ddb7b46636b5c30ce55df12861e72ba1ce2c5a
Alexey Rakitin <alexey.rakitin@babenkocommerce.com>
2023-01-25 14:56:13 +0200

    SAAS-3913: Delivery veriables cover custom PDF

---
+++ Block/Order/DeliveryDateTime.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Calcurates Team
+ * @copyright Copyright © 2020 Calcurates (https://www.calcurates.com)
+ * @license https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @package Calcurates_ModuleMagento
+ */
+
+namespace Calcurates\ModuleMagento\Block\Order;
+
+use Calcurates\ModuleMagento\ViewModel\OrderDeliveryDate;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class DeliveryDateTime extends Template
+{
+    /**
+     * @var OrderDeliveryDate
+     */
+    private $orderDeliveryDate;
+
+    /**
+     * @param Context $context
+     * @param OrderDeliveryDate $orderDeliveryDate
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        OrderDeliveryDate $orderDeliveryDate,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->orderDeliveryDate = $orderDeliveryDate;
+    }
+
+    /**
+     * @return OrderDeliveryDate
+     */
+    public function getOrderDeliveryDate(): OrderDeliveryDate
+    {
+        return $this->orderDeliveryDate;
+    }
+}
