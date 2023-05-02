ac2d5e10e177abf53af73b519de1a27081f7fa6e
Alexey Rakitin <alexey.rakitin@babenkocommerce.com>
2023-05-02 14:52:05 +0300

    CLCR-205: Delivery dates persistence on checkout, OSC compatibility

--- view/frontend/web/js/view/checkout/delivery-date/date-select.js
+++ view/frontend/web/js/view/checkout/delivery-date/date-select.js
@@ -6,200 +6,218 @@
  */
 
 define([
-    'jquery',
-    'ko',
-    'Calcurates_ModuleMagento/js/component/form/element/select',
-    'mage/translate',
-    'Calcurates_ModuleMagento/js/model/delivery-date/delivery-date-list',
-    'Magento_Catalog/js/price-utils',
-    'Magento_Checkout/js/model/quote',
-    'Magento_Checkout/js/action/set-shipping-information'
-], function (
-    $,
-    ko,
-    Select,
-    $t,
-    deliveryDateList,
-    priceUtils,
-    quote,
-    setShippingInformationAction
-) {
-    'use strict';
+    "jquery",
+    "ko",
+    "underscore",
+    "Calcurates_ModuleMagento/js/component/form/element/select",
+    "mage/translate",
+    "Calcurates_ModuleMagento/js/model/delivery-date/delivery-date-list",
+    "Magento_Catalog/js/price-utils",
+    "Magento_Checkout/js/model/quote",
+    "Magento_Checkout/js/action/set-shipping-information",
+    "jquery/jquery-storageapi",
+], function ($, ko, _, Select, $t, deliveryDateList, priceUtils, quote, setShippingInformationAction) {
+    "use strict"
 
     return Select.extend({
         defaults: {
-            caption: $t('Choose a Delivery Date...'),
-            template: 'Calcurates_ModuleMagento/delivery-date/date-select',
-            label: $t('Delivery Date'),
-            additionalCss: '',
-            validationError: '',
+            caption: $t("Choose a Delivery Date..."),
+            template: "Calcurates_ModuleMagento/delivery-date/date-select",
+            label: $t("Delivery Date"),
+            additionalCss: "",
+            validationError: "",
             timeSlotDateRequired: null,
-            defaultValueType: 'earliest',
+            defaultValueType: "earliest",
         },
 
         initObservable: function () {
-            this._super().observe(['options', 'timeSlotDateRequired', 'additionalCss', 'validationError']);
+            this._super().observe(["options", "timeSlotDateRequired", "additionalCss", "validationError"])
 
-            deliveryDateList.currentDeliveryDatesList.subscribe(this.onCurrentDeliveryDatesListChange, this);
-            this.onCurrentDeliveryDatesListChange(deliveryDateList.currentDeliveryDatesList());
-            return this;
+            deliveryDateList.currentDeliveryDatesList.subscribe(this.onCurrentDeliveryDatesListChange, this)
+            this.onCurrentDeliveryDatesListChange(deliveryDateList.currentDeliveryDatesList())
+            return this
         },
 
         onCurrentDeliveryDatesListChange: function (data) {
-            var options = [];
-            this.timeSlotDateRequired(this.getTimeSlotDateRequired(deliveryDateList));
-            this.additionalCss(this.getAdditionalCss());
+            var options = []
+            this.timeSlotDateRequired(this.getTimeSlotDateRequired(deliveryDateList))
+            this.additionalCss(this.getAdditionalCss())
 
-            data.forEach(function (deliveryDate) {
-                options.push({
-                    value: deliveryDate.id,
-                    label: this.dateText(deliveryDate)
-                })
-            }.bind(this));
+            data.forEach(
+                function (deliveryDate) {
+                    options.push({
+                        value: deliveryDate.id,
+                        label: this.dateText(deliveryDate),
+                    })
+                }.bind(this)
+            )
             if (this.timeSlotDateRequired()) {
-                this.caption(null);
+                this.caption(null)
             }
-            this.updateCurrentDate(data);
-            this.setOptions(options);
-            this.setDefaultOption(data);
-            this.updateValue();
-            this.onChangeDate();
+            this.updateCurrentDate(data)
+            this.setOptions(options)
+            this.setDefaultOption(data)
+            this.updateValue()
+            this.onChangeDate()
         },
 
         updateValue: function () {
             if (deliveryDateList.currentDate() && deliveryDateList.currentDate().id) {
-                this.value(deliveryDateList.currentDate().id);
-                this.value.valueHasMutated();
-                return;
+                this.value(deliveryDateList.currentDate().id)
+                this.value.valueHasMutated()
+                return
             }
             if (this.timeSlotDateRequired() && this.options().length) {
-                this.value(this.default);
+                this.value(this.default)
             }
         },
 
         updateCurrentDate: function (data) {
-            var currDate = deliveryDateList.currentDate, newDateItem
+            var currDate = deliveryDateList.currentDate,
+                newDateItem
 
             if (data.length && currDate() && currDate().date) {
                 data.forEach(function (dateSlot) {
-                    if (dateSlot.date.slice(0,10) === currDate().date.slice(0,10)) {
-                        newDateItem = dateSlot;
+                    if (dateSlot.date.slice(0, 10) === currDate().date.slice(0, 10)) {
+                        newDateItem = dateSlot
                     }
                 })
             }
             if (newDateItem && currDate().id !== newDateItem.id) {
-                currDate(newDateItem);
+                currDate(newDateItem)
             } else if (!newDateItem) {
-                currDate({});
+                currDate({})
             }
         },
 
         setDefaultOption: function (data) {
-            var matchedOption
+            var matchedOption, savedOptionId
             if (!data.length) {
-                return;
+                return
             }
-            if (this.defaultValueType === 'earliest_cheapest') {
+            if (localStorage.getItem("calcurates_delivery_date")) {
+                savedOptionId = localStorage.getItem("calcurates_delivery_date")
+                matchedOption = data.find(function (option) {
+                    return option.id === savedOptionId
+                })
+                matchedOption = matchedOption ? matchedOption.id : this.options()[0].value
+            } else if (this.defaultValueType === "earliest_cheapest") {
                 data.sort(function (a, b) {
-                    return Date.parse(a.date) - Date.parse(b.date);
+                    return Date.parse(a.date) - Date.parse(b.date)
                 })
                 matchedOption = data.reduce(function (prev, curr) {
-                    return (prev.fee_amount > curr.fee_amount) ? curr : prev;
+                    return prev.fee_amount > curr.fee_amount ? curr : prev
                 })
-                matchedOption = matchedOption.id;
+                matchedOption = matchedOption.id
             } else {
                 matchedOption = this.options()[0].value
             }
-            this.default = matchedOption;
+            this.default = matchedOption
         },
 
         onUserChangeDate: function () {
-            this.valueChangedByUser = true;
-            this.onChangeDate();
-            this.valueChangedByUser = false;
+            this.valueChangedByUser = true
+            this.onChangeDate()
+            this.valueChangedByUser = false
         },
 
         onChangeDate: function () {
             var currentDateId = this.value(),
-                currentDate;
+                currentDate,
+                firstLoad = _.isEmpty(deliveryDateList.currentDate())
 
             deliveryDateList.currentDeliveryDatesList().forEach(function (date) {
                 if (currentDateId === date.id) {
-                    currentDate = date;
+                    currentDate = date
                 }
-            });
+            })
 
-            deliveryDateList.currentDate(currentDate);
-            if ('undefined' !== typeof deliveryDateList.currentDeliveryDatesList()
-                && deliveryDateList.currentDeliveryDatesList().length > 0
+            deliveryDateList.currentDate(currentDate)
+            if (currentDate) {
+                localStorage.setItem("calcurates_delivery_date", currentDate.id)
+            }
+
+            if (
+                "undefined" !== typeof deliveryDateList.currentDeliveryDatesList() &&
+                deliveryDateList.currentDeliveryDatesList().length > 0
             ) {
-                this.validateSelect();
+                this.validateSelect()
                 /**
                  * Amasty Checkout | Onestepcheckout_Iosc compatibility
                  */
-                if (quote.shippingAddress()
-                    && (window.am_osc_enabled || $('#iosc-summary').length || $('.bss-onestepcheckout').length)
-                    && this.valueChangedByUser
+                if (
+                    (window.am_osc_enabled ||
+                        "iosc_subscribe" in window.checkoutConfig.quoteData ||
+                        $(".bss-onestepcheckout").length) &&
+                    (this.valueChangedByUser || firstLoad)
                 ) {
-                    setShippingInformationAction();
+                    if (quote.shippingAddress()) {
+                        setShippingInformationAction()
+                    } else {
+                        var runOnce = quote.shippingAddress.subscribe(function () {
+                            runOnce.dispose()
+                            setShippingInformationAction()
+                        })
+                    }
                 }
             }
         },
 
         dateText: function (deliveryDate) {
             var optionLabel = deliveryDate.date_formatted,
-                formattedPrice = '';
+                formattedPrice = ""
 
-            optionLabel += ' ' + deliveryDate.label;
+            optionLabel += " " + deliveryDate.label
             if (!deliveryDate.fee_amount_excl_tax) {
-                return optionLabel;
+                return optionLabel
             }
             if (window.checkoutConfig.isDisplayShippingPriceExclTax) {
-                formattedPrice = priceUtils.formatPrice(deliveryDate.fee_amount_excl_tax, quote.getPriceFormat());
-            } else if (window.checkoutConfig.isDisplayShippingBothPrices
-                && deliveryDate.fee_amount_incl_tax !== deliveryDate.fee_amount_excl_tax
+                formattedPrice = priceUtils.formatPrice(deliveryDate.fee_amount_excl_tax, quote.getPriceFormat())
+            } else if (
+                window.checkoutConfig.isDisplayShippingBothPrices &&
+                deliveryDate.fee_amount_incl_tax !== deliveryDate.fee_amount_excl_tax
             ) {
-                formattedPrice = priceUtils.formatPrice(deliveryDate.fee_amount_incl_tax, quote.getPriceFormat())
-                    + ' ' + $t('Excl. Tax') + ': +'
-                    + priceUtils.formatPrice(deliveryDate.fee_amount_excl_tax, quote.getPriceFormat());
+                formattedPrice =
+                    priceUtils.formatPrice(deliveryDate.fee_amount_incl_tax, quote.getPriceFormat()) +
+                    " " +
+                    $t("Excl. Tax") +
+                    ": +" +
+                    priceUtils.formatPrice(deliveryDate.fee_amount_excl_tax, quote.getPriceFormat())
             } else {
-                formattedPrice = priceUtils.formatPrice(deliveryDate.fee_amount_incl_tax, quote.getPriceFormat());
+                formattedPrice = priceUtils.formatPrice(deliveryDate.fee_amount_incl_tax, quote.getPriceFormat())
             }
 
-            optionLabel += ' (+' + formattedPrice + ')';
+            optionLabel += " (+" + formattedPrice + ")"
 
-            return optionLabel;
+            return optionLabel
         },
 
         getTimeSlotDateRequired: function () {
-            var deliveryDatesMetadata = deliveryDateList.getDeliveryDatesMetadata();
+            var deliveryDatesMetadata = deliveryDateList.getDeliveryDatesMetadata()
 
-            return !!(deliveryDatesMetadata
-                && deliveryDatesMetadata.time_slot_date_required === true);
+            return !!(deliveryDatesMetadata && deliveryDatesMetadata.time_slot_date_required === true)
         },
 
         getAdditionalCss: function () {
-            var initialClasses = '';
+            var initialClasses = ""
             if (this.timeSlotDateRequired()) {
-                initialClasses += ' _required';
+                initialClasses += " _required"
             }
             if (this.validationError()) {
-                initialClasses += ' _error';
+                initialClasses += " _error"
             }
-            return initialClasses;
+            return initialClasses
         },
 
         validateSelect: function () {
-            if (this.getTimeSlotDateRequired() === true
-                && 'undefined' === typeof this.value()
-            ) {
-                this.validationError($t('This is a required field.'));
-                this.additionalCss(this.getAdditionalCss());
-                return false;
+            if (this.getTimeSlotDateRequired() === true && "undefined" === typeof this.value()) {
+                this.validationError($t("This is a required field."))
+                this.additionalCss(this.getAdditionalCss())
+                return false
             }
-            this.validationError('');
-            this.additionalCss(this.getAdditionalCss());
-            return true;
-        }
-    });
-});
+            this.validationError("")
+            this.additionalCss(this.getAdditionalCss())
+            return true
+        },
+    })
+})
