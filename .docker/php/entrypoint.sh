eb36ccddb8143be1b581972e6bbb8714e687e27f
Gemorroj <pavel.dyakonov@amasty.com>
2026-03-17 13:20:48 +0300

    build: upd magento

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -12,12 +12,12 @@ done
 
 isSourced=`mariadb --skip-ssl --silent --skip-column-names --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --host="mysql" --port="3306" -e "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$MYSQL_DATABASE';"`
 #isSourced=0
-if [[ -f "/magento2-2.4.8-p3.tar.gz" || "${isSourced}" -eq "0" ]]; then
+if [[ -f "/magento2-2.4.8-p4.tar.gz" || "${isSourced}" -eq "0" ]]; then
     echo "Copying the Magento2 template to the working directory..."
-    tar -zxf "/magento2-2.4.8-p3.tar.gz"
-    rm "/magento2-2.4.8-p3.tar.gz"
-    cp -r ./magento2-2.4.8-p3/* .
-    rm -rf ./magento2-2.4.8-p3
+    tar -zxf "/magento2-2.4.8-p4.tar.gz"
+    rm "/magento2-2.4.8-p4.tar.gz"
+    cp -r ./magento2-2.4.8-p4/* .
+    rm -rf ./magento2-2.4.8-p4
 
     echo "Install composer deps..."
     php -d memory_limit=-1 -d default_socket_timeout=1000 /usr/local/bin/composer update --with-all-dependencies
